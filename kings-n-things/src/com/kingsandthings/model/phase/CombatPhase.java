660ce17b745b23ea86818873fc34798194d0c009
macthere <Mac@Mac-PC>
2014-02-08 00:06:49 -0500

    Added computation for Charging and basic combat calculation to Combat Phase.  Several TODO's added explaining ordering.

--- kings-n-things/src/com/kingsandthings/model/phase/CombatPhase.java
+++ kings-n-things/src/com/kingsandthings/model/phase/CombatPhase.java
@@ -1,47 +1,88 @@
 package com.kingsandthings.model.phase;
+
 import java.util.logging.Logger;
 import com.kingsandthings.model.Player;
 import com.kingsandthings.model.board.Tile;
 import com.kingsandthings.model.things.Creature;
-import java.util.ArrayList;
+import java.util.List;
 
+public class CombatPhase extends Phase {
 
+	private static Logger LOGGER = Logger.getLogger(CombatPhase.class.getName());
 
-public class CombatPhase extends Phase{
+	public CombatPhase() {
+		super("Combat", true, true, 1);
+	}
 
-    private static Logger LOGGER = Logger.getLogger(CombatPhase.class.getName());
+	@Override
+	public void begin() {
+		// Notify listeners that the phase has begun
+		notifyBegin();
 
-    public CombatPhase() {
-        super("Combat", true, true, 1);
-    }
+	}
 
-    @Override
-    public void begin() {
-        // Notify listeners that the phase has begun
-        notifyBegin();
+	// TODO eventually, attackers will need to be a list that can handle multiple attackers at once
+	public void initiateCombat(Player playerDefender, Player playerAttacker,
+			List<Creature> defenders, List<Creature> attackers) {
 
-    }
+		boolean retreat = false;
+		// TODO don't leave this in endless loop!
+		while (!retreat) {
+			
+			int damageToAttacker = computeCombat(playerDefender, defenders);
+			playerRemoveCreatures(playerAttacker, attackers, damageToAttacker);
+			
+			int damageToDefender = computeCombat(playerAttacker, attackers);
+			playerRemoveCreatures(playerDefender, defenders, damageToDefender);
 
-    private int computeCombat(ArrayList<Creature> creatures) {
+			// TODO players must choose if they wish to retreat, in order, starting with attackers
+			/*
+			 * if ((playerAttacker.isRetreat()) || playerDefender.isRetreat()) 
+			 * 	retreat = true; 
+			 * else if (defenders.isEmpty() || attackers.isEmpty())
+			 * 	retreat = true;
+			 */
+		}
 
-        int damageDealt = 0;
-        //get attack damages from defending player's creatures
-        for (Creature creature : creatures) {
-            int combatValue = creature.getCombatValue();
+	}
 
-            //TODO have player actually roll the dice here
-            int diceRoll = rollDice();
-            if (diceRoll >= combatValue) {
-                damageDealt += 1; //Deal one extra damage
-            }
+	// Player chooses set of creatures that receive damage hits, call this
+	// function to remove them
+	private void playerRemoveCreatures(Player player, List<Creature> creatures, int damage) {
+		// TODO player removes number of creatures of their choice = damage
+		// player.remove([all chosen creatures]);
+	}
 
-        }
-        return damageDealt;
-    }
+	// TODO add in ability order of operations, ie Magic-->Ranged-->Melee respectively
+	// Returns amount of damage a set of creatures deal based on die rolls
+	private int computeCombat(Player player, List<Creature> creatures) {
 
-    //Not needed once player actually rolls dice
-    private int rollDice() {
-        return (int)(6.0 * Math.random()) + 1; //return # between 1-6
-    }
+		int damageDealt = 0;
+		int diceRoll;
+		// get attack damages from defending player's creatures
+		for (Creature creature : creatures) {
+			int combatValue = creature.getCombatValue();
+
+			// TODO have player actually roll the dice here
+			// CHARGE allows creature to roll twice
+			if (creature.getAbilities().contains("CHARGE")) {
+				diceRoll = rollDice();
+				if (diceRoll >= combatValue) {
+					damageDealt += 1;
+				}
+			}
+
+			diceRoll = rollDice();
+			if (diceRoll >= combatValue) {
+				damageDealt += 1;
+			}
+		}
+		return damageDealt;
+	}
+
+	// Not needed once player actually rolls dice
+	private int rollDice() {
+		return (int) (6.0 * Math.random()) + 1; // return # between 1-6
+	}
+
 }
-
